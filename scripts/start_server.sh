58611d3c843cab5d7206d090a596a372a6ca2bf5
shjeon <shjeon0730@gmail.com>
2018-08-11 01:55:45 -0500

    change the scripts

--- scripts/start_server.sh
+++ scripts/start_server.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 cd /home/ec2-user/stella-api
-export PORT=80
+export PORT=8080
 export NODE_ENV=production
-export SERVER_URL=1{amazon-server-ip}
+export SERVER_URL=18.223.49.113
+export SERVER_PRIVATE_URL=172.31.35.53
 forever start server.js
--- server.js
+++ server.js
@@ -6,7 +6,9 @@ const testPort = 5000;
 
 var port = process.env.PORT || testPort;
 const server = http.createServer(app);
-
-server.listen(port, ret => {
+var ip = process.env.SERVER_PRIVATE_URL
+  ? process.env.SERVER_PRIVATE_URL
+  : "127.0.0.1";
+server.listen(port, ip, ret => {
   console.log(`Express: port ${port} is listening...`);
 });
